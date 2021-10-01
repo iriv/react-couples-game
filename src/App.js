79a71c38f311fdb15f212b7774ce34f2c326a97b
Илья Рысенков <irysenkov@imaqliq.com>
2021-10-01 14:44:37 +0300

    change app

--- src/App.js
+++ src/App.js
@@ -2,15 +2,17 @@ import React from "react";
 // import logo from './logo.svg';
 //import "./App.css";
 
-var arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-
-var payrsIndexes = [];
-
-var tempVal = null;
-var tempKey = null;
-var obj = null;
-
-var clickCount = 1;
+var arr = [1, 2, 3, 4]
+var payrsIndexes = []
+var tempVal = null
+var tempKey = null
+var obj = null
+var timer
+var clickCount = 1
+var delay = 500
+var tempArr = []
+let counter = 0;
+let time = 0;
 
 class Item extends React.Component {
   constructor(props) {
@@ -19,37 +21,49 @@ class Item extends React.Component {
     this.show = this.show.bind(this);
   }
 
-  show() {
-    if (payrsIndexes.indexOf(this.props.number) !== -1) return;
-    this.setState((state) => ({ active: !state.active }));
+  async check() {
+    console.log('fn check')
 
-    if (clickCount === 2) {
-      if (this.props.val === obj.props.val) {
-        // console.log("suc", this.props.val, tempVal, tempKey);
-        payrsIndexes.push(obj.props.number);
-        payrsIndexes.push(this.props.number);
-        this.setState((state) => ({ active: true }));
-
-        if (payrsIndexes.length / 2 == arr.length) {
-          alert("Ура!");
-          window.location.reload();
+    await new Promise((resolve, reject) => {
+      setTimeout(() => {
+            for (let elem of tempArr) {
+              elem.setState((state) => ({ active: false }));
         }
+        resolve()
+      }, delay)
+    })
+
+    tempArr = []
+  }
+
+ show() {
+    //debugger;
+    //console.log('tempArr.length = ' + tempArr.length)
+
+    if (time == 0){
+      setInterval(()=>{
+        time++
+      }, 1000)
+    }
+
+    if (this.state.active == true) return;
+
+    if (!tempArr.includes(this) && tempArr.length < 2) {
+      this.setState((state) => ({ active: true }))
+      tempArr.push(this)
+    }
+    
+    if (tempArr.length == 2) {
+      if (tempArr[0].props.val !== tempArr[1].props.val) {
+        this.check()
       } else {
-        // console.log("err", this.props.number, this.props.val, tempVal, tempKey);
-        setTimeout(
-          function () {
-            obj.setState((state) => ({ active: false }));
-            this.setState((state) => ({ active: false }));
-          }.bind(this),
-          1000
-        );
+        counter++
+        if (counter == arr.length) {
+          alert('Your time is ' + time)
+          document.location.reload()
+        } 
+        tempArr = []
       }
-      clickCount = 1;
-      //this.setState((state) => ({ active: !state.active }));
-    } else {
-      obj = this;
-      clickCount++;
-      // this.setState((state) => ({ active: !state.active }));
     }
   }
 
@@ -58,7 +72,7 @@ class Item extends React.Component {
       <div
         className={this.state.active ? "active" : "noactive"}
         onClick={this.show}
-      >
+        >
         {" "}
         {/* используем id в качестве ключа */}
         <span>{this.props.val}</span>
@@ -69,17 +83,23 @@ class Item extends React.Component {
 }
 
 class Items extends React.Component {
-  render() {
-    // var arr  = _.fill(Array(this.props.data.length*2), 1)
-    var i = 0,
-      arr = [];
 
-    while (i < 2) {
-      this.props.data.forEach((item) => {
-        arr.push(item);
-      });
-      i++;
+   shuffle(array) {
+    let currentIndex = array.length,  randomIndex;
+
+    while (currentIndex != 0) {
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+
+      [array[currentIndex], array[randomIndex]] = [
+        array[randomIndex], array[currentIndex]];
     }
+
+    return array;
+  }
+
+  render() {
+    let arr = this.shuffle(this.props.data.concat(this.props.data))
 
     const tpl = arr.map(function (item, i) {
       return <Item val={item} key={i} number={i} />;
